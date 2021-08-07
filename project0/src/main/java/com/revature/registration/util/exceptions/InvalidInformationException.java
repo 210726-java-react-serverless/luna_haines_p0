894a0565be5bff9f3f358298a2db02bfa8b860dd
LunaHaines <nhaines95@gmail.com>
2021-08-07 14:39:01 -0600

    created invalid information exception

---
+++ project0/src/main/java/com/revature/registration/util/exceptions/InvalidInformationException.java
@@ -0,0 +1,8 @@
+package com.revature.registration.util.exceptions;
+
+public class InvalidInformationException extends RuntimeException {
+
+    public InvalidInformationException(String message) {
+        super(message);
+    }
+}
