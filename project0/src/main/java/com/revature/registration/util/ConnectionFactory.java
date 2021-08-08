d5593d5c39fe309aa19034a9fafa2ae75f66f449
LunaHaines <nhaines95@gmail.com>
2021-08-08 16:06:22 -0600

    created singleton factory to connect with database

---
+++ project0/src/main/java/com/revature/registration/util/ConnectionFactory.java
@@ -0,0 +1,8 @@
+package com.revature.registration.util;
+
+import com.mongodb.client.MongoClient;
+
+public class MongoClientFactory {
+    private final MongoClient mongoClient;
+    private static final com.mongodb.client.MongoClientFactory
+}
