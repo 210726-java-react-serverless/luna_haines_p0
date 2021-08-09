1d970ecf649aaa3ac0e84ae28eac15d8fd766c22
LunaHaines <nhaines95@gmail.com>
2021-08-08 19:43:54 -0600

    created a Course model

---
+++ project0/src/main/java/com/revature/registration/models/Course.java
@@ -0,0 +1,66 @@
+package com.revature.registration.models;
+
+public class Course {
+    public String id;
+    public String number;
+    public String name;
+    public String description;
+    public String professorEmail;
+    public String[] studentEmails;
+
+    Course(String number, String name, String description, String professorEmail, String... studentEmails) {
+        this.number = number;
+        this.name = name;
+        this.description = description;
+        this.professorEmail = professorEmail;
+        this.studentEmails = studentEmails;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getProfessorEmail() {
+        return professorEmail;
+    }
+
+    public void setProfessorEmail(String professorEmail) {
+        this.professorEmail = professorEmail;
+    }
+
+    public String[] getStudentEmails() {
+        return studentEmails;
+    }
+
+    public void setStudentEmails(String[] studentEmails) {
+        this.studentEmails = studentEmails;
+    }
+}
