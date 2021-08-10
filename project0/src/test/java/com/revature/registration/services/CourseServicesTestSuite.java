aa8cbaf174163dc411ea32a5cdfb69d2343c082e
LunaHaines <nhaines95@gmail.com>
2021-08-10 15:52:11 -0600

    setup before and after methods in test suites

--- project0/src/test/java/com/revature/registration/services/CourseServicesTestSuite.java
+++ project0/src/test/java/com/revature/registration/services/CourseServicesTestSuite.java
@@ -2,10 +2,24 @@ package com.revature.registration.services;
 
 import com.revature.registration.repositories.CourseRepository;
 import com.revature.registration.util.Session;
+import org.junit.*;
+import static org.mockito.Mockito.*;
 
 public class CourseServicesTestSuite {
     CourseServices sut;
 
     private Session mockSession;
     private CourseRepository mockCourseRepo;
+
+    @Before
+    public void beforeTests() {
+        mockSession = mock(Session.class);
+        mockCourseRepo = mock(CourseRepository.class);
+        sut = new CourseServices(mockCourseRepo);
+    }
+
+    @After
+    public void afterTests() {
+        sut = null;
+    }
 }
--- project0/src/test/java/com/revature/registration/services/UserServicesTestSuite.java
+++ project0/src/test/java/com/revature/registration/services/UserServicesTestSuite.java
@@ -3,6 +3,8 @@ package com.revature.registration.services;
 import com.revature.registration.repositories.FacultyRepository;
 import com.revature.registration.repositories.StudentRepository;
 import com.revature.registration.util.Session;
+import org.junit.*;
+import static org.mockito.Mockito.*;
 
 public class UserServicesTestSuite {
 
@@ -11,4 +13,20 @@ public class UserServicesTestSuite {
     private Session mockSession;
     private StudentRepository mockStudentRepo;
     private FacultyRepository mockFacultyRepo;
+
+    @Before
+    public void beforeTests() {
+        mockSession = mock(Session.class);
+        mockStudentRepo = mock(StudentRepository.class);
+        mockFacultyRepo = mock(FacultyRepository.class);
+        sut = new UserServices(mockStudentRepo,mockFacultyRepo);
+    }
+
+    @After
+    public void afterTests() {
+        sut = null;
+    }
+
+
+
 }
