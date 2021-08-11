55dbc6b6607094aa782df17acef5e760b91159c4
LunaHaines <nhaines95@gmail.com>
2021-08-10 23:20:57 -0600

    wrote documentation for utilities and App

--- project0/src/main/java/com/revature/registration/util/exceptions/ResourcePersistenceException.java
+++
@@ -1,8 +0,0 @@
-package com.revature.registration.util.exceptions;
-
-public class ResourcePersistenceException extends RuntimeException {
-
-    public ResourcePersistenceException(String message) {
-        super(message);
-    }
-}
