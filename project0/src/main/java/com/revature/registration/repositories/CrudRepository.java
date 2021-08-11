ca5e5369b2328530b4e0a8e69290fc40f0b7c74e
LunaHaines <nhaines95@gmail.com>
2021-08-10 22:20:56 -0600

    wrote documentation for repository classes

--- project0/src/main/java/com/revature/registration/repositories/CrudRepository.java
+++ project0/src/main/java/com/revature/registration/repositories/CrudRepository.java
@@ -1,5 +1,10 @@
 package com.revature.registration.repositories;
 
+/**
+ * This interface defines CRUD (create, read, update, delete) repositories. Creation is ensured by the save method,
+ * reading by findById, updating by update, and deletion by deleteById
+ * @param <T>
+ */
 public interface CrudRepository<T> {
     T findById(String id);
     T save(T newResource);
